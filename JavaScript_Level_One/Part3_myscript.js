55ba0c868d6ee885dfa63e078ada22930405359c
Sai sam Phyo linn <saisamphyolinn@gmail.com>
2020-08-11 19:16:53 +0800

    basic Js

---
+++ JavaScript_Level_One/Part3_myscript.js
@@ -0,0 +1,4 @@
+var weight = prompt("What is the weight in pounds (lbs)?");
+var convr = weight * 0.454;
+alert("That is: "+convr+" kilograms");
+console.log("Conversion Completed!")
---
+++ JavaScript_Level_One/myScript.js
@@ -0,0 +1,4 @@
+alert("Welcome to your bank");
+var deposit = prompt("How much would you like to deposit?");
+alert("You have deposited: "+deposit);
+console.log("you are a cool person!");
